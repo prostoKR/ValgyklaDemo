f4b2aef938f345e0a93ad0593fca7772f33bc884
KonstantinR <konstantinrub@gmail.com>
2022-06-27 20:37:26 +0300

    projekto generavimas

---
+++ backend_valgykla_demo/src/main/java/lt/vtmc/valgykla_demo/ValgyklaDemoApplication.java
@@ -0,0 +1,13 @@
+package lt.vtmc.valgykla_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValgyklaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValgyklaDemoApplication.class, args);
+	}
+
+}
